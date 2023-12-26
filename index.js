92be9c48ac6882498acc59c9550f7ffd9acb4880
Sakshi Sharma <113548910+Sakshi-888@users.noreply.github.com>
2023-12-26 10:52:09 +0530

    learning js

---
+++ index.js
@@ -0,0 +1,10 @@
+let routine=["cricket","eat","library","restday","sports","spiritual"];
+
+for(let i=0;i<10;i++)
+{
+    console.log(`week ${i+1}`);
+    for(let j=0;j<6;j++)
+    {
+        console.log(`DAY ${j+1} = ${routine[j]}`);
+        }
+}
